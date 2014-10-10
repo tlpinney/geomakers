01802db18ff05f008866e388d6f68aa6661a1d47
DanielJDufour <daniel.j.dufour@gmail.com>
2014-10-09 21:41:49 -0400

    donate page

--- windwaker/skyshaker/views.py
+++ windwaker/skyshaker/views.py
@@ -5,4 +5,4 @@ def index(request):
     return render(request, 'skyshaker/index.html')
 
 def donate(request):
-    return render(request, 'skyshaker/index.html')
+    return render(request, 'skyshaker/donate.html')
