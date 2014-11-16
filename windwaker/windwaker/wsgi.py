8ab45904159a057cfe8e9a2868aa3f18542f9419
Travis L Pinney <travis.pinney@gmail.com>
2014-11-16 16:01:03 -0500

    Use settings.py

--- windwaker/windwaker/wsgi.py
+++ windwaker/windwaker/wsgi.py
@@ -8,7 +8,7 @@ https://docs.djangoproject.com/en/dev/howto/deployment/wsgi/
 """
 
 import os
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "windwaker.settings_dev")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "windwaker.settings")
 
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
