0923ddb32cdec8c087b6f22d5d89832c088972be
Tyler Garner <garnertb@gmail.com>
2014-11-16 16:25:06 -0500

    Add the staticfiles_dirs setting.

--- windwaker/windwaker/settings.py
+++ windwaker/windwaker/settings.py
@@ -107,6 +107,10 @@ STATIC_URL = '/static/'
 
 TEMPLATE_DIRS = [os.path.join(BASE_DIR, 'templates')]
 
+STATICFILES_DIRS = (
+     os.path.join(BASE_DIR, 'static'),
+)
+
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media') #absolute path to the media directory
 
